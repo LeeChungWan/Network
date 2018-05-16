c00b4ba355b4256aaa8a4c8d61a923ec02e6f68d
leechungwan <cnddhks846@naver.com>
2018-05-16 11:47:25 +0900

    NAK, Timeout function 추가!

--- UDP/StopandWait_ARQ/sender.py
+++ UDP/StopandWait_ARQ/sender.py
@@ -43,7 +43,8 @@ while 1:
 		read_file.close()
 		break
 	except socket.timeout as e:
-		print("Time Out!!!")
+		print(" * Time Out!! ***")
+		print("Retransmission File Info...")
 		sender_sock.sendto(info_packet, (receiverIP, receiverPort))
 
 
@@ -51,24 +52,28 @@ read_file = open("./"+file_name, "rb")
 
 #sending data.
 while current_size != total_size:
-	h = hashlib.sha1()
-	data = read_file.read(1024)
-	checksum = str(seq_num).encode() + data
-	h.update(checksum)
-	data_packet = h.digest() + str(seq_num).encode() + data
-	sender_sock.sendto(data_packet, (receiverIP, receiverPort))
-	#receive ACK
-	ACK = sender_sock.recv(1)
-	while ACK.decode() == str(NAK) :
-		print(" * Received NAK - Retransmit!")
+	try:
+		h = hashlib.sha1()
+		data = read_file.read(1024)
+		checksum = str(seq_num).encode() + data
+		h.update(checksum)
+		data_packet = h.digest() + str(seq_num).encode() + data
 		sender_sock.sendto(data_packet, (receiverIP, receiverPort))
+		#receive ACK
 		ACK = sender_sock.recv(1)
-	if len(data) <1024 :
-		current_size += len(data)
-		print("(corrent size / total size) = " + str(current_size) + "/" + str(total_size) + " , 100.0%")
-	else :
-		current_size += 1024
+		while ACK.decode() == str(NAK) :
+			print(" * Received NAK - Retransmit!")
+			sender_sock.sendto(data_packet, (receiverIP, receiverPort))
+			ACK = sender_sock.recv(1)
+		if len(data) <1024 :
+			current_size += len(data)
+		else :
+			current_size += 1024
 		print("(corrent size / total size) = " + str(current_size) + "/" + str(total_size) + " , " + str(round(current_size/total_size*100, 3))+ "%")
+	except socket.timeout ad e:
+		print(" * Time Out!! ***")
+		print("Retransmission : (corrent size / total size) = " + str(current_size) + "/" + str(total_size) + " , " + str(round(current_size/total_size*100, 3))+ "%")
+		sender_sock.sendto(data_packet, (receiverIP, receiverPort))
 	
 
 read_file.close()
